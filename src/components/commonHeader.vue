02570fd0e6b774b8a96b7e6eefbdbc8d91f7ab70
wunianli <2823884385@qq.com>
2019-06-11 18:09:14 +0800

    公共头部

--- src/components/commonHeader.vue
+++ src/components/commonHeader.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="headWrap">
     <p class="top"></p>
+    <div class="container">
+      <div class="w1200">
+        <div><img src="../assets/imgs/logo2.png" alt=""></div>
+        <ul>
+          <li>4577</li>
+          <li>4577</li>
+          <li>4577</li>
+          <li>4577</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'commonHeader',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+
+    }
+  },
 }
 </script>
 
@@ -21,5 +34,24 @@ export default {
     background: #DCB26F;
     width: 100%;
   }
+  > div {
+    background: #17161D;
+    > div {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 20px 0;
+      ul {
+        width: 60%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        li {
+          color: #DCB26F;
+        }
+      }
+    }
+  }
+
 }
 </style>
